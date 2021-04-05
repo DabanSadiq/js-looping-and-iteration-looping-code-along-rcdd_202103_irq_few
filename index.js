970999e58fe02a999a4f13219ca1a176a017f0bc
Daban Sadiq <dabansadiq@icloud.com>
2021-04-05 15:21:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const gifts = ["teddy bear", "drone", "doll"];
 function writeCards(names,event) {
   let array = [];
   for (let i = 0; i < names.length; i++) {
+<<<<<<< HEAD
     array [i] =`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+=======
+    array [i] =`Thank you, ${names[i]}, for the wonderful ${event}  gift!`;
+>>>>>>> e8b4d364168f0a34070d79369d0cb37a8d258ecd
   }
 
   return array;
 }
 
+<<<<<<< HEAD
 writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
 
 
@@ -22,3 +27,6 @@ function countDown(num){
   }
 }
 countDown(10);
+=======
+writeCards(["Lisa", "Kaitlin", "Jan"], "birthday");
+>>>>>>> e8b4d364168f0a34070d79369d0cb37a8d258ecd
